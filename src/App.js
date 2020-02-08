ab7893da8c9402f9f937dfa684045c14e90d3555
Luiza <luiza.mendes@m4u.com.br>
2020-02-08 19:32:54 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  // const myHeaders = new Headers({
+  //   Accept: "application/json",
+  //   authorization:
+  //     "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImI0M2UyNGRhLWQwZGQtNDM5YS04YWJjLTViOWU5ZWEyMjRjOSIsImlhdCI6MTU3NzczNTUxMSwic3ViIjoiZGV2ZWxvcGVyL2U5NWM0NDk0LWU1NTctMGFhNC1lMzA5LTU0YjNmMTUwMmJkZSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTc5LjIxOC4xNjguMTYiXSwidHlwZSI6ImNsaWVudCJ9XX0.8g7IDVG_JjpyOwAUFA7sSHM-d_eB7pzGI45AWUO4HZuww5GqNZWa4FMAyddXD6A-wx_1frQdnhwlRHJoyTR9Lw",
+  //   "Access-Control-Allow-Origin": "*",
+  //   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+  // });
+  // // curl -X GET --header 'Accept: application/json' --header "authorization: Bearer <API token>" 'https://api.brawlstars.com/v1/players/%2322CJ0R8Q'
+
+  // const myInit = {
+  //   method: "GET",
+  //   headers: myHeaders,
+  //   mode: "cors",
+  //   cache: "default"
+  // };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/player/id").then(response => {
+      response.json().then(json => {
+        console.log(json);
+      });
+    });
+  }, []);
+  return <div className="App"></div>;
+};
 
 export default App;
