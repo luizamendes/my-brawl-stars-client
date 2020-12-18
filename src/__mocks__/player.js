14555ad6460ec958aa717caecb01f3707204f4fc
Luiza Mendes <Luiza.Mendes@ibm.com>
2020-12-18 12:43:14 -0300

    Adding mock

---
+++ src/__mocks__/player.js
@@ -0,0 +1,838 @@
+export const player = {
+  tag: "#22CJ0R8Q",
+  name: "LuMee",
+  nameColor: "0xffa8e132",
+  icon: {
+    id: 28000045,
+  },
+  trophies: 19979,
+  highestTrophies: 20565,
+  powerPlayPoints: 373,
+  highestPowerPlayPoints: 810,
+  expLevel: 158,
+  expPoints: 128786,
+  isQualifiedFromChampionshipChallenge: false,
+  "3vs3Victories": 7521,
+  soloVictories: 76,
+  duoVictories: 273,
+  bestRoboRumbleTime: 5,
+  bestTimeAsBigBrawler: 0,
+  club: {
+    tag: "#J8GJ02G",
+    name: "Los Cordeiros",
+  },
+  brawlers: [
+    {
+      id: 16000000,
+      name: "SHELLY",
+      power: 10,
+      rank: 22,
+      trophies: 541,
+      highestTrophies: 613,
+      starPowers: [
+        {
+          id: 23000135,
+          name: "BAND-AID",
+        },
+        {
+          id: 23000076,
+          name: "SHELL SHOCK",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000255,
+          name: "FAST FORWARD",
+        },
+      ],
+    },
+    {
+      id: 16000001,
+      name: "COLT",
+      power: 10,
+      rank: 21,
+      trophies: 512,
+      highestTrophies: 561,
+      starPowers: [
+        {
+          id: 23000077,
+          name: "SLICK BOOTS",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000273,
+          name: "SPEEDLOADER",
+        },
+      ],
+    },
+    {
+      id: 16000002,
+      name: "BULL",
+      power: 9,
+      rank: 20,
+      trophies: 516,
+      highestTrophies: 526,
+      starPowers: [],
+      gadgets: [
+        {
+          id: 23000272,
+          name: "T-BONE INJECTOR",
+        },
+      ],
+    },
+    {
+      id: 16000003,
+      name: "BROCK",
+      power: 10,
+      rank: 21,
+      trophies: 513,
+      highestTrophies: 569,
+      starPowers: [
+        {
+          id: 23000079,
+          name: "INCENDIARY",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000245,
+          name: "ROCKET LACES",
+        },
+      ],
+    },
+    {
+      id: 16000004,
+      name: "RICO",
+      power: 10,
+      rank: 21,
+      trophies: 525,
+      highestTrophies: 582,
+      starPowers: [
+        {
+          id: 23000156,
+          name: "ROBO RETREAT",
+        },
+        {
+          id: 23000080,
+          name: "SUPER BOUNCY",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000246,
+          name: "MULTIBALL LAUNCHER",
+        },
+      ],
+    },
+    {
+      id: 16000005,
+      name: "SPIKE",
+      power: 10,
+      rank: 22,
+      trophies: 555,
+      highestTrophies: 640,
+      starPowers: [
+        {
+          id: 23000151,
+          name: "CURVEBALL",
+        },
+        {
+          id: 23000081,
+          name: "FERTILIZE",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000247,
+          name: "POPPING PINCUSHION",
+        },
+      ],
+    },
+    {
+      id: 16000006,
+      name: "BARLEY",
+      power: 10,
+      rank: 21,
+      trophies: 527,
+      highestTrophies: 595,
+      starPowers: [
+        {
+          id: 23000158,
+          name: "EXTRA NOXIOUS",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000250,
+          name: "STICKY SYRUP MIXER",
+        },
+      ],
+    },
+    {
+      id: 16000007,
+      name: "JESSIE",
+      power: 10,
+      rank: 22,
+      trophies: 618,
+      highestTrophies: 647,
+      starPowers: [
+        {
+          id: 23000149,
+          name: "SHOCKY",
+        },
+        {
+          id: 23000083,
+          name: "ENERGIZE",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000251,
+          name: "SPARK PLUG",
+        },
+      ],
+    },
+    {
+      id: 16000008,
+      name: "NITA",
+      power: 10,
+      rank: 22,
+      trophies: 533,
+      highestTrophies: 627,
+      starPowers: [
+        {
+          id: 23000136,
+          name: "HYPER BEAR",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000249,
+          name: "BEAR PAWS",
+        },
+      ],
+    },
+    {
+      id: 16000009,
+      name: "DYNAMIKE",
+      power: 10,
+      rank: 22,
+      trophies: 512,
+      highestTrophies: 642,
+      starPowers: [
+        {
+          id: 23000155,
+          name: "DEMOLITION",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000258,
+          name: "FIDGET SPINNER",
+        },
+      ],
+    },
+    {
+      id: 16000010,
+      name: "EL PRIMO",
+      power: 10,
+      rank: 21,
+      trophies: 527,
+      highestTrophies: 597,
+      starPowers: [
+        {
+          id: 23000140,
+          name: "METEOR RUSH",
+        },
+        {
+          id: 23000086,
+          name: "EL FUEGO",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000264,
+          name: "SUPLEX SUPPLEMENT",
+        },
+      ],
+    },
+    {
+      id: 16000011,
+      name: "MORTIS",
+      power: 10,
+      rank: 21,
+      trophies: 525,
+      highestTrophies: 592,
+      starPowers: [
+        {
+          id: 23000154,
+          name: "COILED SNAKE",
+        },
+        {
+          id: 23000087,
+          name: "CREEPY HARVEST",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000265,
+          name: "COMBO SPINNER",
+        },
+      ],
+    },
+    {
+      id: 16000012,
+      name: "CROW",
+      power: 10,
+      rank: 22,
+      trophies: 513,
+      highestTrophies: 623,
+      starPowers: [
+        {
+          id: 23000143,
+          name: "CARRION CROW",
+        },
+        {
+          id: 23000088,
+          name: "EXTRA TOXIC",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000243,
+          name: "DEFENSE BOOSTER",
+        },
+      ],
+    },
+    {
+      id: 16000013,
+      name: "POCO",
+      power: 10,
+      rank: 22,
+      trophies: 557,
+      highestTrophies: 624,
+      starPowers: [
+        {
+          id: 23000089,
+          name: "DA CAPO!",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000267,
+          name: "TUNING FORK",
+        },
+      ],
+    },
+    {
+      id: 16000014,
+      name: "BO",
+      power: 10,
+      rank: 24,
+      trophies: 515,
+      highestTrophies: 738,
+      starPowers: [
+        {
+          id: 23000148,
+          name: "SNARE A BEAR",
+        },
+        {
+          id: 23000090,
+          name: "CIRCLING EAGLE",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000263,
+          name: "SUPER TOTEM",
+        },
+      ],
+    },
+    {
+      id: 16000015,
+      name: "PIPER",
+      power: 10,
+      rank: 21,
+      trophies: 525,
+      highestTrophies: 569,
+      starPowers: [
+        {
+          id: 23000152,
+          name: "SNAPPY SNIPING",
+        },
+        {
+          id: 23000091,
+          name: "AMBUSH",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000268,
+          name: "AUTO AIMER",
+        },
+        {
+          id: 23000291,
+          name: "HOMEMADE RECIPE",
+        },
+      ],
+    },
+    {
+      id: 16000016,
+      name: "PAM",
+      power: 10,
+      rank: 22,
+      trophies: 525,
+      highestTrophies: 614,
+      starPowers: [
+        {
+          id: 23000139,
+          name: "MAMA'S SQUEEZE",
+        },
+        {
+          id: 23000092,
+          name: "MAMA'S HUG",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000257,
+          name: "PULSE MODULATOR",
+        },
+      ],
+    },
+    {
+      id: 16000017,
+      name: "TARA",
+      power: 10,
+      rank: 21,
+      trophies: 571,
+      highestTrophies: 571,
+      starPowers: [
+        {
+          id: 23000160,
+          name: "HEALING SHADE",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000266,
+          name: "PSYCHIC ENHANCER",
+        },
+      ],
+    },
+    {
+      id: 16000018,
+      name: "DARRYL",
+      power: 10,
+      rank: 21,
+      trophies: 525,
+      highestTrophies: 590,
+      starPowers: [
+        {
+          id: 23000094,
+          name: "STEEL HOOPS",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000260,
+          name: "RECOILING ROTATOR",
+        },
+      ],
+    },
+    {
+      id: 16000019,
+      name: "PENNY",
+      power: 10,
+      rank: 22,
+      trophies: 585,
+      highestTrophies: 623,
+      starPowers: [
+        {
+          id: 23000142,
+          name: "BALLS OF FIRE",
+        },
+        {
+          id: 23000099,
+          name: "LAST BLAST",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000248,
+          name: "POCKET DETONATOR",
+        },
+      ],
+    },
+    {
+      id: 16000020,
+      name: "FRANK",
+      power: 9,
+      rank: 21,
+      trophies: 519,
+      highestTrophies: 580,
+      starPowers: [],
+      gadgets: [
+        {
+          id: 23000261,
+          name: "ACTIVE NOISE CANCELING",
+        },
+      ],
+    },
+    {
+      id: 16000021,
+      name: "GENE",
+      power: 10,
+      rank: 21,
+      trophies: 525,
+      highestTrophies: 576,
+      starPowers: [
+        {
+          id: 23000159,
+          name: "SPIRIT SLAP",
+        },
+        {
+          id: 23000109,
+          name: "MAGIC PUFFS",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000252,
+          name: "LAMP BLOWOUT",
+        },
+      ],
+    },
+    {
+      id: 16000022,
+      name: "TICK",
+      power: 10,
+      rank: 22,
+      trophies: 527,
+      highestTrophies: 615,
+      starPowers: [
+        {
+          id: 23000161,
+          name: "AUTOMA-TICK RELOAD",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000253,
+          name: "BACKUP MINE",
+        },
+      ],
+    },
+    {
+      id: 16000023,
+      name: "LEON",
+      power: 10,
+      rank: 20,
+      trophies: 537,
+      highestTrophies: 537,
+      starPowers: [
+        {
+          id: 23000141,
+          name: "INVISIHEAL",
+        },
+        {
+          id: 23000119,
+          name: "SMOKE TRAILS",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000276,
+          name: "CLONE PROJECTOR",
+        },
+      ],
+    },
+    {
+      id: 16000024,
+      name: "ROSA",
+      power: 10,
+      rank: 21,
+      trophies: 533,
+      highestTrophies: 596,
+      starPowers: [
+        {
+          id: 23000147,
+          name: "THORNY GLOVES",
+        },
+        {
+          id: 23000124,
+          name: "PLANT LIFE",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000242,
+          name: "GROW LIGHT",
+        },
+      ],
+    },
+    {
+      id: 16000025,
+      name: "CARL",
+      power: 10,
+      rank: 20,
+      trophies: 544,
+      highestTrophies: 544,
+      starPowers: [
+        {
+          id: 23000145,
+          name: "PROTECTIVE PIROUETTE",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000262,
+          name: "HEAT EJECTOR",
+        },
+      ],
+    },
+    {
+      id: 16000026,
+      name: "BIBI",
+      power: 10,
+      rank: 21,
+      trophies: 525,
+      highestTrophies: 563,
+      starPowers: [
+        {
+          id: 23000146,
+          name: "BATTING STANCE",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000275,
+          name: "VITAMIN BOOSTER",
+        },
+      ],
+    },
+    {
+      id: 16000027,
+      name: "8-BIT",
+      power: 10,
+      rank: 21,
+      trophies: 525,
+      highestTrophies: 585,
+      starPowers: [
+        {
+          id: 23000168,
+          name: "Boosted Booster",
+        },
+        {
+          id: 23000181,
+          name: "EXTRA LIFE",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000259,
+          name: "CHEAT CARTRIDGE",
+        },
+      ],
+    },
+    {
+      id: 16000028,
+      name: "SANDY",
+      power: 10,
+      rank: 22,
+      trophies: 543,
+      highestTrophies: 626,
+      starPowers: [
+        {
+          id: 23000186,
+          name: "RUDE SANDS",
+        },
+        {
+          id: 23000187,
+          name: "HEALING WINDS",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000270,
+          name: "SLEEP STIMULATOR",
+        },
+      ],
+    },
+    {
+      id: 16000029,
+      name: "BEA",
+      power: 10,
+      rank: 23,
+      trophies: 634,
+      highestTrophies: 692,
+      starPowers: [
+        {
+          id: 23000192,
+          name: "INSTA BEALOAD",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000271,
+          name: "HONEY MOLASSES",
+        },
+      ],
+    },
+    {
+      id: 16000030,
+      name: "EMZ",
+      power: 10,
+      rank: 26,
+      trophies: 615,
+      highestTrophies: 800,
+      starPowers: [
+        {
+          id: 23000199,
+          name: "HYPE",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000274,
+          name: "FRIENDZONER",
+        },
+      ],
+    },
+    {
+      id: 16000031,
+      name: "MR. P",
+      power: 10,
+      rank: 22,
+      trophies: 525,
+      highestTrophies: 606,
+      starPowers: [
+        {
+          id: 23000204,
+          name: "HANDLE WITH CARE",
+        },
+        {
+          id: 23000205,
+          name: "REVOLVING DOOR",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000269,
+          name: "SERVICE BELL",
+        },
+      ],
+    },
+    {
+      id: 16000032,
+      name: "MAX",
+      power: 10,
+      rank: 21,
+      trophies: 533,
+      highestTrophies: 555,
+      starPowers: [
+        {
+          id: 23000210,
+          name: "SUPER CHARGED",
+        },
+      ],
+      gadgets: [],
+    },
+    {
+      id: 16000034,
+      name: "JACKY",
+      power: 9,
+      rank: 22,
+      trophies: 533,
+      highestTrophies: 605,
+      starPowers: [],
+      gadgets: [
+        {
+          id: 23000256,
+          name: "PNEUMATIC BOOSTER",
+        },
+      ],
+    },
+    {
+      id: 16000035,
+      name: "GALE",
+      power: 10,
+      rank: 22,
+      trophies: 579,
+      highestTrophies: 622,
+      starPowers: [
+        {
+          id: 23000228,
+          name: "BLUSTERY BLOW",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000277,
+          name: "SPRING EJECTOR",
+        },
+      ],
+    },
+    {
+      id: 16000036,
+      name: "NANI",
+      power: 10,
+      rank: 12,
+      trophies: 189,
+      highestTrophies: 189,
+      starPowers: [
+        {
+          id: 23000235,
+          name: "TEMPERED STEEL",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000278,
+          name: "WARP BLAST",
+        },
+      ],
+    },
+    {
+      id: 16000037,
+      name: "SPROUT",
+      power: 10,
+      rank: 21,
+      trophies: 560,
+      highestTrophies: 579,
+      starPowers: [
+        {
+          id: 23000241,
+          name: "PHOTOSYNTHESIS",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000244,
+          name: "GARDEN MULCHER",
+        },
+      ],
+    },
+    {
+      id: 16000038,
+      name: "SURGE",
+      power: 10,
+      rank: 15,
+      trophies: 313,
+      highestTrophies: 317,
+      starPowers: [
+        {
+          id: 23000283,
+          name: "TO THE MAX!",
+        },
+      ],
+      gadgets: [
+        {
+          id: 23000285,
+          name: "POWER SURGE",
+        },
+      ],
+    },
+  ],
+};
